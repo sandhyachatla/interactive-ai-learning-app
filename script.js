b21485e46b94b795d564cae78ba38f1c2a422182
sandhyachatla <chatlasandhya06@gmail.com>
2026-06-12 00:27:01 -0700

    Add AI question answering functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Interactive AI Learning App");
+function askAI(){
+
+let question =
+document.getElementById("question").value;
+
+let answer = "";
+
+if(question.toLowerCase().includes("photosynthesis")){
+answer =
+"Photosynthesis is the process by which plants make food using sunlight.";
+}
+
+else if(question.toLowerCase().includes("newton")){
+answer =
+"Newton's First Law states that an object remains at rest or in motion unless acted upon by an external force.";
+}
+
+else{
+answer =
+"AI Tutor is still learning. More topics will be added soon.";
+}
+
+document.getElementById("answer").innerHTML = answer;
+
+}
