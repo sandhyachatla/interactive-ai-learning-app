663209267a13bdbdc59e0b77db258b4b82f07d4f
sandhyachatla <chatlasandhya06@gmail.com>
2026-06-12 03:02:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -132,5 +132,7 @@ document.getElementById("quizResult").innerHTML =
 
 document.getElementById("score").innerHTML =
 score;
+document.getElementById("totalScore").innerHTML = score;
 
+document.getElementById("progress").innerHTML = score * 10;
 }
